81b422631263100ea9b97e7601e8f39f61850dfa
Reymond Julio <reymondjulio58@gmail.com>
2023-08-07 16:00:15 +0700

    feat: add footer to layout folder

---
+++ app/components/layout/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="w-full">
+      <div className="bg-slate-200 flex justify-center items-end w-full border-t-2 border-blue-200 p-4">
+        <p className="text-center">
+          All rights reserved <span>&copy; </span> Reymond Julio 2023.
+        </p>
+      </div>
+    </footer>
+  );
+}
