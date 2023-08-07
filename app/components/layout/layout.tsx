32c89e3de18241f60e7d937631c91925ac553d0e
Reymond Julio <reymondjulio58@gmail.com>
2023-08-07 19:55:59 +0700

    feat: add typeScript typings for children prop in Layout component

--- app/components/layout/layout.tsx
+++ app/components/layout/layout.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Header from "./header";
 import Footer from "./footer";
 
-export default function Layout({ Children }) {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col justify-between min-h-screen">
       <Header />
-      <main>{Children}</main>
+      <main>{children}</main>
       <Footer />
     </div>
   );
