1b78633ad9d464d72d2813dc5a9cf4f5bb3c38c6
Reymond Julio <reymondjulio58@gmail.com>
2023-08-11 15:31:28 +0700

    style: improve login page layout

--- app/routes/login._index.tsx
+++ app/routes/login._index.tsx
@@ -5,53 +5,32 @@ import Button from "~/components/ui/button";
 
 export default function Login() {
   return (
-    <div className="container mx-auto w-full max-w-6xl p-4 gap-x-6 gap-y-12 flex flex-wrap justify-center lg:justify-between lg:items-center min-h-screen">
-      <div className="max-w-lg w-full space-y-4 text-center lg:text-left self-center lg:mb-44">
-        <h1 className="text-4xl lg:text-6xl text-blue-500 font-bold">
-          Friendbook
-        </h1>
-        <p className="text-2xl lg:text-3xl font-semibold">
-          Connect with friends and the world around you on Friendbook.
-        </p>
+    <div className="container mx-auto w-full h-full max-w-6xl h-screen p-4 gap-x-6 gap-y-6 flex flex-wrap justify-center lg:justify-between items-start lg:items-center">
+      <div className="max-w-lg w-full space-y-4 text-center lg:text-left self-center  lg:mb-44">
+        <h1 className="text-4xl sm:text-6xl text-blue-500 font-bold">Friendbook</h1>
+        <p className="text-2xl sm:text-3xl font-semibold">Connect with friends and the world around you on Friendbook.</p>
       </div>
 
-      <div className="max-w-sm w-full ring-0 mb-24">
-        <Form method="POST" className="flex flex-col gap-y-4 bg-white p-6">
-          <div className="w-full">
-            <Label htmlFor="email" />
-            <Input
-              id="email"
-              name="email"
-              type="text"
-              autoComplete="email"
-              required
-              placeholder="Email or Phone Number"
-            />
+      <div className="max-w-sm w-full ring-0 lg:mb-24">
+        <div className="bg-white">
+          <Form method="POST" className="flex flex-col gap-y-4 px-12 pt-10 mb-4">
+            <div className="w-full">
+              <Label htmlFor="email" />
+              <Input id="email" name="email" type="text" autoComplete="email" required placeholder="Email or Phone Number" />
+            </div>
+            <div>
+              <Label htmlFor="password" />
+              <Input id="password" name="password" type="text" autoComplete="password" required placeholder="Password" />
+            </div>
+
+            <Button>Log in</Button>
+          </Form>
+
+          <div className="flex px-12 pb-8">
+            <Link to="/register" className="bg-green-500 hover:bg-green-400 py-2 rounded text-white text-sm font-bold w-full text-center self-center">
+              Create New Account
+            </Link>
           </div>
-          <div>
-            <Label htmlFor="password" />
-            <Input
-              id="password"
-              name="password"
-              type="text"
-              autoComplete="password"
-              required
-              placeholder="Password"
-            />
-          </div>
-
-          <Button>Log in</Button>
-        </Form>
-
-        <div>
-          <p className="text-center text-blue-500">Forgot password?</p>
-
-          <Link
-            to="/register"
-            className="bg-green-500 hover:bg-green-400 py-2 rounded text-white text-sm font-bold w-full text-center self-center"
-          >
-            Create New Account
-          </Link>
         </div>
       </div>
     </div>
