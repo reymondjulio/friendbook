3e3426864c0a07ae4551c2ee4eae2955125a68f4
Reymond Julio <reymondjulio58@gmail.com>
2023-08-11 15:20:08 +0700

    refactor: remove min-h-screen class from main tag

--- app/components/layout/layout.tsx
+++ app/components/layout/layout.tsx
@@ -8,9 +8,9 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex flex-col justify-between bg-slate-100 min-h-screen">
+    <div className="bg-slate-100">
       <Header />
-      <main className="container py-2 px-4 mx-auto max-w-6xl min-h-screen w-full">{children}</main>
+      <main className="container py-2 px-4 mx-auto max-w-6xl w-full">{children}</main>
       <Footer />
     </div>
   );
