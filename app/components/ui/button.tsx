b0577cfee789b82b5695634b73ab75cdec3d9b07
Reymond Julio <reymondjulio58@gmail.com>
2023-08-13 16:07:12 +0700

    style: improve button on the username page

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -2,18 +2,11 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: string;
 }
 
-export default function Button({
-  variant = "primary",
-  children,
-  ...props
-}: ButtonProps) {
+export default function Button({ variant = "primary", children, ...props }: ButtonProps) {
   return (
     <button
-      className={`p-2 rounded w-full self-center ${
-        (variant === "primary" &&
-          `bg-blue-500 text-white text-sm sm:text-md hover:bg-blue-400`) ||
-        (variant === "secondary" &&
-          `bg-slate-300 text-black text-sm sm:text-md hover:bg-slate-200`)
+      className={`py-2 px-4 flex items-center justify-center gap-x-2 rounded w-full self-center ${
+        (variant === "primary" && `bg-blue-500 text-white text-sm sm:text-md hover:bg-blue-400`) || (variant === "secondary" && `bg-slate-300 text-black text-sm sm:text-md hover:bg-slate-200`)
       }`}
       {...props}
     >
