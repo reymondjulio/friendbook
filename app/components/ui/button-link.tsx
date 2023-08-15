b2c0f2b102f2b4dac84f9e4d45d5ce5a25981904
Reymond Julio <reymondjulio58@gmail.com>
2023-08-15 18:05:41 +0700

    refactor: link props with typescript

--- app/components/ui/button-link.tsx
+++ app/components/ui/button-link.tsx
@@ -1,9 +1,8 @@
+import type { LinkProps } from "@remix-run/react";
 import { Link } from "@remix-run/react";
 
-interface AnchorProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {}
-
-export default function ButtonLink({ children, ...props }: AnchorProps) {
-  <Link to={""} className="w-full block bg-green-500 hover:bg-green-400 py-2 rounded text-white text-sm font-bold text-center" {...props}>
+export default function ButtonLink({ children, ...props }: LinkProps) {
+  <Link className="w-full block bg-green-500 hover:bg-green-400 py-2 rounded text-white text-sm font-bold text-center" {...props}>
     {children}
   </Link>;
 }
