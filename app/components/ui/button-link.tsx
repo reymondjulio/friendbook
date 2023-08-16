fac7ab90b9afac9d309a51b1aaeded1d8d9cc362
Reymond Julio <reymondjulio58@gmail.com>
2023-08-16 12:46:00 +0700

    fix: add a return function

--- app/components/ui/button-link.tsx
+++ app/components/ui/button-link.tsx
@@ -2,7 +2,9 @@ import type { LinkProps } from "@remix-run/react";
 import { Link } from "@remix-run/react";
 
 export default function ButtonLink({ children, ...props }: LinkProps) {
-  <Link className="w-full block bg-green-500 hover:bg-green-400 py-2 rounded text-white text-sm font-bold text-center" {...props}>
-    {children}
-  </Link>;
+  return (
+    <Link className="w-full block bg-green-500 hover:bg-green-400 py-2 rounded text-white text-sm font-bold text-center" {...props}>
+      {children}
+    </Link>
+  );
 }
