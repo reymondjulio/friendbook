d7bd87a0ffec510c4e57b2f4706ed802dc8deb92
Reymond Julio <reymondjulio58@gmail.com>
2023-08-17 16:33:39 +0700

    style: put a link outside the image friend card

--- app/routes/friends._index.tsx
+++ app/routes/friends._index.tsx
@@ -18,36 +18,26 @@ export default function Route() {
       <ul className="max-w-6xl mx-auto grid grid-cols-2 justify-center items-center md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-4 py-4">
         {friends.map((friend) => {
           return (
-            <Link
-              className="block hover:opacity-80 justify-self-center self-center"
-              key={friend.id}
-              to={`/friends/${friend.username}`}
-            >
-              <li className="max-w-fit h-fit rounded bg-white" key={friend.id}>
-                <div>
-                  <img
-                    className="w-60 h-60 rounded object-cover overflow-hidden"
-                    src={friend.avatarURL}
-                    alt={friend.name}
-                  />
-                </div>
-                <div className="p-2 text-center">
-                  <h2 className="text-lg font-semibold">{friend.name}</h2>
-                  <p className="text-slate-80">{friend.age} years old</p>
-                  <p className="text-gray-500">
-                    {friend.mutualFriends.length} mutual friends
-                  </p>
-                </div>
-                <div className="flex flex-col p-2 gap-y-2">
-                  <Form method="PUT">
-                    <Button variant="primary">Confirm</Button>
-                  </Form>
-                  <Form method="PUT">
-                    <Button variant="secondary">Delete</Button>
-                  </Form>
-                </div>
-              </li>
-            </Link>
+            <li className="max-w-fit h-fit rounded bg-white justify-self-center self-center" key={friend.id}>
+              <div>
+                <Link className="block hover:opacity-80" key={friend.id} to={`/friends/${friend.username}`}>
+                  <img className="w-60 h-60 rounded object-cover overflow-hidden" src={friend.avatarURL} alt={friend.name} />
+                </Link>
+              </div>
+              <div className="p-2 text-center">
+                <h2 className="text-lg font-semibold">{friend.name}</h2>
+                <p className="text-slate-80">{friend.age} years old</p>
+                <p className="text-gray-500">{friend.mutualFriends.length} mutual friends</p>
+              </div>
+              <div className="flex flex-col p-2 gap-y-2">
+                <Form method="PUT">
+                  <Button variant="primary">Confirm</Button>
+                </Form>
+                <Form method="PUT">
+                  <Button variant="secondary">Delete</Button>
+                </Form>
+              </div>
+            </li>
           );
         })}
       </ul>
