276d244dcfdb768dc31c20ce862a1ae9f4ab3b72
Reymond Julio <reymondjulio58@gmail.com>
2023-08-19 13:34:37 +0700

    chore: setup data for users and posts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,12 +9,29 @@ async function main() {
       {
         name: "Reymond Julio",
         username: "reymond",
+        avatarURL: "/images/avatars/reymond.jpg",
+        coverURL: "/images/cover/reymond-cover.jpg",
       },
       {
         name: "M Haidar Hanif",
         username: "haidar",
+        avatarURL: "/images/avatars/haidar.jpg",
+        coverURL: "/images/cover/haidar-cover.jpg",
+      },
+      {
+        name: "Caroline Elizabeth",
+        username: "caroline",
+        avatarURL: "/images/avatars/caroline.jpg",
+        coverURL: "/images/cover/caroline-cover.jpg",
       },
     ],
+  });
+  await prisma.post.deleteMany();
+  await prisma.post.create({
+    data: { text: "Halo teman-teman", user: { connect: { username: "reymond" } } },
+  });
+  await prisma.post.create({
+    data: { text: "Ayo belajar", user: { connect: { username: "haidar" } } },
   });
 }
 
