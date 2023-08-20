79f11afa0434fe0e2f48a8a63d8e668663abe637
Reymond Julio <reymondjulio58@gmail.com>
2023-08-20 14:20:26 +0700

    style: styling layout users page

--- app/routes/users.tsx
+++ app/routes/users.tsx
@@ -15,8 +15,18 @@ export const loader = async ({ request }: LoaderArgs) => {
 export default function RouteComponent() {
   const { users } = useLoaderData<typeof loader>();
   return (
-    <div>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
+    <div className="container mx-auto max-w-6xl p-6 h-screen">
+      <ul className="grid grid-cols-3">
+        {users.map((user) => {
+          return (
+            <li key={user.id} className="max-w-fit h-fit bg-white">
+              {user.avatarURL && <img className="w-60 h-60 rounded object-cover overflow-hidden" src={user.avatarURL} alt={user.name} />}
+              <p className="text-center font-semibold p-4">{user.name}</p>
+              <p className="text-center font-light">@{user.username}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
