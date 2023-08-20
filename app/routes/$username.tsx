bdb39ffed0f434d513b6df68b4958cefaccaed17
Reymond Julio <reymondjulio58@gmail.com>
2023-08-20 14:22:18 +0700

    feat: setup load username

--- app/routes/$username.tsx
+++ app/routes/$username.tsx
@@ -1,13 +1,16 @@
-import type { LoaderArgs } from "@remix-run/node";
+import { json, type LoaderArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { prisma } from "~/db.server";
 
-export const loader = async ({ request }: LoaderArgs) => {
-  return null;
+export const loader = async ({ params }: LoaderArgs) => {
+  const user = await prisma.user.findUnique({
+    where: { username: params.username },
+    include: { posts: true },
+  });
+  return json({ user });
 };
 
-export default function RouteComponent(){
-  const data = useLoaderData<typeof loader>()
-  return (
-    <div />
-  );
+export default function RouteComponent() {
+  const { user } = useLoaderData<typeof loader>();
+  return <pre>{JSON.stringify(user, null, 2)}</pre>;
 }
