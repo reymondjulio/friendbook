181efc288423cdd5b8085c00cf5c7bbfa6db8070
Reymond Julio <reymondjulio58@gmail.com>
2023-09-06 14:19:12 +0700

    style: change the radius button to rounded lg

--- app/components/ui/button-link.tsx
+++ app/components/ui/button-link.tsx
@@ -3,7 +3,7 @@ import { Link } from "@remix-run/react";
 
 export default function ButtonLink({ children, ...props }: LinkProps) {
   return (
-    <Link className="w-full block bg-blue-500 hover:bg-blue-400 py-2 rounded text-white text-sm font-bold text-center" {...props}>
+    <Link className="w-full block bg-blue-500 hover:bg-blue-400 py-2 rounded-lg text-white text-sm font-bold text-center" {...props}>
       {children}
     </Link>
   );
--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export default function Button({ variant = "primary", children, ...props }: ButtonProps) {
   return (
     <button
-      className={`py-2 px-4 flex items-center justify-center gap-x-2 rounded w-full self-center ${
+      className={`py-2 px-4 flex items-center justify-center gap-x-2 rounded-lg w-full self-center ${
         (variant === "primary" && `bg-green-500 text-white font-bold text-sm sm:text-md hover:bg-green-400`) ||
         (variant === "secondary" && `bg-blue-500 text-white font-bold text-sm sm:text-md hover:bg-blue-400`) ||
         (variant === "tertiary" && `bg-slate-300 text-black text-sm sm:text-md hover:bg-slate-200`)
