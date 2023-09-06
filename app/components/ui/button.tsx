44a2919d94d4cdb36545360bf3fe08f7fd4113ec
Reymond Julio <reymondjulio58@gmail.com>
2023-09-06 12:01:42 +0700

    style: change variant button

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -6,7 +6,9 @@ export default function Button({ variant = "primary", children, ...props }: Butt
   return (
     <button
       className={`py-2 px-4 flex items-center justify-center gap-x-2 rounded w-full self-center ${
-        (variant === "primary" && `bg-blue-500 text-white font-bold text-sm sm:text-md hover:bg-blue-400`) || (variant === "secondary" && `bg-slate-300 text-black text-sm sm:text-md hover:bg-slate-200`)
+        (variant === "primary" && `bg-green-500 text-white font-bold text-sm sm:text-md hover:bg-green-400`) ||
+        (variant === "secondary" && `bg-blue-500 text-white font-bold text-sm sm:text-md hover:bg-blue-400`) ||
+        (variant === "tertiary" && `bg-slate-300 text-black text-sm sm:text-md hover:bg-slate-200`)
       }`}
       {...props}
     >
