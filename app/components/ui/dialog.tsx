0d984fffd7df9a3d06ee1b4d14ceb9b88421e27b
Reymond Julio <reymondjulio58@gmail.com>
2023-09-07 14:12:23 +0700

    style: improve styling dialog user post

--- app/components/ui/dialog.tsx
+++ app/components/ui/dialog.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { Form } from "@remix-run/react";
 import Button from "~/components/ui/button";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 export default function DialogPosts() {
   return (
@@ -9,28 +10,27 @@ export default function DialogPosts() {
         <button className="w-full hover:bg-slate-100 rounded-full text-gray-600 p-4 text-left bg-slate-200 font-semibold leading-none focus:outline-none">What's on your mind?</button>
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
-        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+        <Dialog.Overlay className="bg-slate-200 bg-opacity-70 data-[state=open]:animate-overlayShow fixed inset-0" />
+        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-1/2 left-1/2 max-h-[85vh] w-[90vw] max-w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white p-6 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
           <Form className="w-full" method="POST">
-            <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">Create post</Dialog.Title>
-            <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal"></Dialog.Description>
-
-            <div className="mb-[15px] flex items-center gap-5">
-              <label className="text-violet11 w-[90px] text-right text-[15px] hidden" htmlFor="message">
+            <Dialog.Title className="text-center mb-4 text-lg font-medium">Create post</Dialog.Title>
+            <Dialog.Description></Dialog.Description>
+            <hr className="w-full mt-6" />
+            <div className="mb-4 flex items-center gap-5">
+              <label className="text-black hidden" htmlFor="message">
                 Message
               </label>
-              <textarea className="text-black text-left outline-none p-0 inline-flex h-[100px] w-full flex-1 items-center justify-center rounded-lg text-lg" id="message" placeholder="What's on your mind?" name="message" />
+              <textarea className="text-black text-left outline-none p-2 inline-flex h-36 w-full flex-1 items-center justify-center rounded-lg text-lg" id="message" placeholder="What's on your mind?" name="message" />
             </div>
-            <div className="mt-[25px] flex justify-end">
+            <div className="mt-6 flex">
               <Button variant="secondary" type="submit">
                 Post
               </Button>
             </div>
             <Dialog.Close asChild>
-              <button
-                className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
-                aria-label="Close"
-              ></button>
+              <button className="absolute top-5 right-4 inline-flex p-2 appearance-none items-center justify-center rounded-full bg-slate-300 hover:bg-slate-200 focus:outline-none" aria-label="Close">
+                <XMarkIcon className="w-6 h-6 text-gray-500"></XMarkIcon>
+              </button>
             </Dialog.Close>
           </Form>
         </Dialog.Content>
