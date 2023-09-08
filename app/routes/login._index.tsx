ddc13044938c3d0054d9de5db54bb8a837c95f95
M Haidar Hanif <git@mhaidarhanif.com>
2023-09-08 15:05:39 +0700

    feat: user login and check password

--- app/routes/login._index.tsx
+++ app/routes/login._index.tsx
@@ -4,25 +4,49 @@ import Input from "~/components/ui/input";
 import Button from "~/components/ui/button";
 import ButtonLink from "~/components/ui/button-link";
 import type { ActionArgs } from "@remix-run/node";
+import { prisma } from "~/db.server";
+
+import bcrypt from "bcrypt";
 
 export default function Login() {
   return (
     <div className="container mx-auto w-full max-w-6xl h-screen p-4 gap-x-6 gap-y-6 flex flex-wrap justify-center lg:justify-between items-start lg:items-center">
       <div className="max-w-lg w-full space-y-4 text-center lg:text-left self-center  lg:mb-44">
-        <h1 className="text-4xl sm:text-6xl text-blue-500 font-bold">Friendbook</h1>
-        <p className="text-2xl sm:text-3xl font-semibold">Connect with friends and the world around you on Friendbook.</p>
+        <h1 className="text-4xl sm:text-6xl text-blue-500 font-bold">
+          Friendbook
+        </h1>
+        <p className="text-2xl sm:text-3xl font-semibold">
+          Connect with friends and the world around you on Friendbook.
+        </p>
       </div>
 
       <div className="max-w-sm w-full ring-0 shadow-md lg:mb-24">
         <div className="bg-white">
-          <Form method="POST" className="flex flex-col gap-y-4 px-12 pt-10 mb-4">
+          <Form
+            method="POST"
+            className="flex flex-col gap-y-4 px-12 pt-10 mb-4"
+          >
             <div>
               <Label htmlFor="email" />
-              <Input id="email" name="email" type="text" autoComplete="email" required placeholder="Email address" />
+              <Input
+                id="email"
+                name="email"
+                type="text"
+                autoComplete="email"
+                required
+                placeholder="Email address"
+              />
             </div>
             <div>
               <Label htmlFor="password" />
-              <Input id="password" name="password" type="text" autoComplete="password" required placeholder="Password" />
+              <Input
+                id="password"
+                name="password"
+                type="text"
+                autoComplete="password"
+                required
+                placeholder="Password"
+              />
             </div>
 
             <Button variant="secondary" type="submit">
@@ -42,10 +66,19 @@ export default function Login() {
 export async function action({ request }: ActionArgs) {
   const formData = await request.formData();
 
-  const email = formData.get("email");
-  const password = formData.get("password");
+  const email = String(formData.get("email"));
+  const password = String(formData.get("password"));
 
-  console.log({ email, password });
+  const user = await prisma.user.findUnique({
+    where: { email },
+    include: { password: true },
+  });
+  if (!user?.password) return null;
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password.hash);
+  if (isPasswordCorrect) return null;
+
+  console.log({ user });
 
   return null;
 }
