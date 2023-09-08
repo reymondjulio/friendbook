14ed7b5373fd3bdd7524cfba791f196752db9d15
M Haidar Hanif <git@mhaidarhanif.com>
2023-09-08 14:51:16 +0700

    feat: seed users with password

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,41 @@
 import { PrismaClient } from "@prisma/client";
 import dataUsersJSON from "./users.json";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 async function main() {
   await prisma.user.deleteMany();
-  await prisma.user.createMany({
-    data: dataUsersJSON,
+  // await prisma.user.createMany({
+  //   data: dataUsersJSON,
+  // });
+
+  const dataUsersWithPassword = dataUsersJSON.map((user) => {
+    const hash = bcrypt.hashSync(user.password ?? "", 10);
+
+    const newUser = {
+      ...user,
+      password: { create: { hash } },
+    };
+
+    return newUser;
   });
+  // console.log(JSON.stringify(dataUsersWithPassword, null, 2));
+
+  for (const user of dataUsersWithPassword) {
+    const createdUser = await prisma.user.create({
+      data: user,
+    });
+
+    console.info(`✅ User "${createdUser.username}" created`);
+  }
+
   await prisma.post.deleteMany();
   await prisma.post.create({
-    data: { text: "Halo teman-teman", user: { connect: { username: "reymond" } } },
+    data: {
+      text: "Halo teman-teman",
+      user: { connect: { username: "reymond" } },
+    },
   });
   await prisma.post.create({
     data: { text: "Ayo belajar", user: { connect: { username: "haidar" } } },
