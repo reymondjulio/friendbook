db6fc72e64f2d3a23ae690b66063bcc0fe12855c
Reymond Julio <reymondjulio58@gmail.com>
2023-09-12 14:56:08 +0700

    feat: add user session on nav items

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -17,17 +17,13 @@ const navItemsAuthenticated = [
 export default function Header() {
   const { userSession } = useRootLoaderData();
 
-  const navItems = userSession?.id
-    ? navItemsAuthenticated
-    : navItemsUnauthenticated;
+  const navItems = userSession?.id ? navItemsAuthenticated : navItemsUnauthenticated;
 
   return (
     <header className="w-full bg-blue-500 sticky top-0 z-10">
       <nav className="container flex flex-wrap max-w-6xl py-4 md:py-2 sm:px-14 md:px-12 lg:px-8 justify-center md:justify-normal gap-y-4 gap-x-4 items-center">
         <Link to="/">
-          <h1 className="text-3xl text-white font-bold lg:pl-2 md:mr-4">
-            Friendbook
-          </h1>
+          <h1 className="text-3xl text-white font-bold lg:pl-2 md:mr-4">Friendbook</h1>
         </Link>
 
         <div className="md:mr-12">
@@ -35,11 +31,7 @@ export default function Header() {
             <label className="hidden" htmlFor="search">
               search
             </label>
-            <input
-              className="bg-slate-100 rounded-full focus:outline-none py-2 px-8 text-sm text-center items-stretch"
-              type="search"
-              placeholder="Search Friends"
-            />
+            <input className="bg-slate-100 rounded-full focus:outline-none py-2 px-8 text-sm text-center items-stretch" type="search" placeholder="Search Friends" />
           </form>
         </div>
 
@@ -47,10 +39,7 @@ export default function Header() {
           {navItems.map((navItem) => {
             return (
               <li key={navItem.to}>
-                <Link
-                  className="text-white font-semibold text-lg"
-                  to={navItem.to}
-                >
+                <Link className="text-white font-semibold text-lg" to={navItem.to}>
                   {navItem.text}
                 </Link>
               </li>
