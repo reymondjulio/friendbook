d480bb5417d3319b36b6ddbda98709e0ac6ff76e
Reymond Julio <reymondjulio58@gmail.com>
2023-09-12 15:00:00 +0700

    style: improve user layout on mobile screen

--- app/routes/users.tsx
+++ app/routes/users.tsx
@@ -15,15 +15,15 @@ export const loader = async ({ request }: LoaderArgs) => {
 export default function RouteComponent() {
   const { users } = useLoaderData<typeof loader>();
   return (
-    <div className="container mx-auto max-w-6xl px-6 py-4 min-h-screen">
-      <ul className="grid grid-cols-4 gap-x-6 gap-y-6">
+    <div className="container mx-auto max-w-6xl md:px-6 py-4 min-h-screen">
+      <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-x-2 md:gap-x-4 lg:gap-x-6 gap-y-6">
         {users.map((user) => {
           return (
             <li key={user.id} className="max-w-fit h-fit">
               <Link to={`/${user.username}`}>{user.avatarURL && <img className="w-60 h-60 rounded-t-lg object-cover overflow-hidden bg-white hover:opacity-80" src={user.avatarURL} alt={user.name} />}</Link>
               <div className=" bg-white p-4 space-y-2">
-                <p className="text-center font-semibold">{user.name}</p>
-                <p className="text-center font-light">@{user.username}</p>
+                <p className="text-center text-xs md:text-base font-semibold">{user.name}</p>
+                <p className="text-center text-sm md:text-base font-light">@{user.username}</p>
               </div>
             </li>
           );
